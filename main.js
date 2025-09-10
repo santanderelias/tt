1a08160addec2d0435371389256ee484846b91b6
Elias Santander <cazagb@gmail.com>
2025-09-10 14:19:59 +0000

    small tweaks

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 console.log('ready!');
+const infoDiv = document.querySelector('.info');
+function updateState() {
+    infoDiv.textContent = 'Ready!';
+    console.log('State updated');
+}
+updateState();
