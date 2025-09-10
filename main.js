bfa08344efd685b94efdc5a5fbb263167afd70f6
Elias Santander <cazagb@gmail.com>
2025-09-10 14:36:56 +0000

    small tweaks

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 console.log('ready!');
 const infoDiv = document.querySelector('.info');
 const button = document.querySelector('.butonera button');
-function updateState() {
+function updateText() {
     infoDiv.textContent = 'Ready!';
+    console.log('Text updated');
+};
+const updateState = () => {
     console.log('State updated');
-}
+};
+button.addEventListener('click', updateState);
 updateState();
-//button.addEventListener('click', updateState);
