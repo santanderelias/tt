fc81b51cf2f878a650f742a30660e93a77b356f9
Elias Santander <cazagb@gmail.com>
2025-09-10 15:18:23 +0000

    small tweaks

--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 const infoDiv = document.querySelector('.info');
 const button = document.querySelector('.butonera button');
+function updateClock() {
+    const now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+
+    // Add leading zeros if hours or minutes are less than 10
+    hours = hours.toString().padStart(2, '0');
+    minutes = minutes.toString().padStart(2, '0');
+
+    const timeString = `${hours}:${minutes}`;
+    document.getElementById('clock').innerHTML = timeString;
+}
+
+  // Update the clock immediately and then every second
+updateClock();
+setInterval(updateClock, 1000); // Update every 1000 milliseconds (1 second)
 function updateToolbarStatusText() {
     infoDiv.textContent = 'Ready!';
     console.log('Text updated');
